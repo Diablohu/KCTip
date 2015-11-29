23a799927c7da2761b20dabf7192e34581a2ee26
Diablohu <diablohudream@gmail.com>
2015-11-30 00:34:17 +0800

    preserve comments

--- dist/kctip.js
+++ dist/kctip.js
@@ -1,5 +1,29 @@
 'use strict';
 
+/*
+
+Current status
+
+* Tip layout done
+* Tip content NYI
+* Browser compatibility NYI
+
+ */
+
+/*
+div#kctip.kctip
+	div.wrapper
+		CONTENT
+*/
+
+/*
+polyfill to do
+
+* transition event
+* classList
+* requestAnimationFrame
+*/
+
 (function () {
 	"use strict";
 
@@ -7,10 +31,13 @@
 	    _x = undefined,
 	    _y = undefined;
 
+	/* style */
+	// STYLE will be replaced with CSS
 	var style = document.createElement('style');
 	style.innerHTML = '#kctip{z-index:100;position:absolute;display:none;top:-1000px;left:-1000px;color:#f2f2f2;background:rgba(32,32,32,.85);font-size:14px;line-height:150%;opacity:0;cursor:default!important;-webkit-transition:opacity .2s ease-out;transition:opacity .2s ease-out;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none;-webkit-box-shadow:0 5px 5px rgba(0,0,0,.35);box-shadow:0 5px 5px rgba(0,0,0,.35);max-width:320px}#kctip>.wrapper{display:block;position:relative;z-index:1;border:1px solid rgba(255,255,255,.5);padding:4px 6px}#kctip.mod-blur-backdrop,#kctip.mod-blur-backdrop>.wrapper{background:rgba(32,32,32,.5)}#kctip.on{opacity:1}#kctip.on.mod-blur-backdrop{-webkit-backdrop-filter:blur(7.5px);backdrop-filter:blur(7.5px)}#kctip.show{display:block;will-change:opacity}#kctip:before{position:absolute;width:0;height:0;overflow:hidden;content:"";border:5px solid transparent}#kctip .loading{padding:.25em .75em}#kctip[kctip-indicator-pos=bottom]:before{border-top-color:rgba(255,255,255,.5);left:50%;left:-webkit-calc(50% - 4px);left:calc(50% - 4px);bottom:-10px}#kctip[kctip-indicator-pos=bottom]{-webkit-box-shadow:0 -5px 5px rgba(0,0,0,.35);box-shadow:0 -5px 5px rgba(0,0,0,.35)}#kctip[kctip-indicator-pos=top]:before{border-bottom-color:rgba(255,255,255,.5);left:50%;left:-webkit-calc(50% - 4px);left:calc(50% - 4px);top:-10px}#kctip[kctip-indicator-pos=left]:before{border-right-color:rgba(255,255,255,.5);top:50%;top:-webkit-calc(50% - 4px);top:calc(50% - 4px);left:-10px}#kctip[kctip-indicator-pos=right]:before{border-left-color:rgba(255,255,255,.5);top:50%;top:-webkit-calc(50% - 4px);top:calc(50% - 4px);right:-10px}';
 	document.head.appendChild(style);
 
+	/* makesure mouseover triggered by mouse not touch */
 	var _preventMouseover = !1,
 	    _isTouch = !1;
 
@@ -25,7 +52,7 @@
 			_isTouch = !1;
 		}
 	});
-
+	//document.addEventListener("mouseenter", function mouseoverPreventMouseover(e){
 	document.addEventListener("mouseover", function mouseoverPreventMouseover(e) {
 		if (_isTouch) {
 			_isTouch = !1;
@@ -34,17 +61,25 @@
 			_preventMouseover = !1;
 		}
 	});
+	//document.addEventListener("mouseleave", function mouseleavePreventMouseover(e){
+	//	})
 
 	var KCTip = {
+		//is_init:			false,
+		//is_showing:		false,
+		// curLoading: 		null,
 		pos: 'bottom',
-
+		//pos:				'mouse',
+		//w:				0,
+		//h:				0,
 		size_indicator: 8,
+		cache: {},
 
+		// content filters
 		filters: [],
 
+		// delay to hide tip
 		countdown_fade: 250,
-
-		cache: {},
 
 		init: function init() {
 			if (this.is_init) return this;
@@ -56,9 +91,7 @@
 			evts.forEach(function (evt) {
 				this.body.addEventListener(evt, function (e) {
 					var style = window.getComputedStyle ? getComputedStyle(e.target, null) : e.target.currentStyle;
-					if (e.currentTarget == e.target && e.propertyName == 'opacity' && style.opacity == 0) {
-						KCTip.hideAfter();
-					}
+					if (e.currentTarget == e.target && e.propertyName == 'opacity' && style.opacity == 0) KCTip.hideAfter();
 				});
 			}, this);
 			document.body.appendChild(this.body);
@@ -67,13 +100,16 @@
 			this.container.classList.add('wrapper');
 			this.body.appendChild(this.container);
 
-			if ('backdrop-filter' in document.documentElement.style || '-webkit-backdrop-filter' in document.documentElement.style) {
-				this.body.addClass('mod-blur-backdrop');
-			}
+			// backdrop-filter
+			if ('backdrop-filter' in document.documentElement.style || '-webkit-backdrop-filter' in document.documentElement.style) this.body.addClass('mod-blur-backdrop');
 
 			this.is_init = !0;
 		},
 
+		// 显示
+		// el:		element that trigger the tip
+		// cont: 	HTML code or element node
+		// pos:		tip position, top || bottom || right || left
 		show: function show(el, cont, pos) {
 			if (_preventMouseover || !el) return !1;
 
@@ -81,43 +117,37 @@
 
 			el = el || document.body;
 			this.el = el;
-
 			pos = pos || el.getAttribute('kctip-position') || this.pos;
-
 			cont = this.content(cont);
 
 			this.init();
 
-			if (!this.body.classList.contains('show')) {
-				this.body.classList.add('show');
-			}
+			if (!this.body.classList.contains('show')) this.body.classList.add('show');
 
 			this.position(cont, pos);
-
 			this.is_showing = !0;
 		},
 
+		// 计算tip位置
 		position: function position(cont, pos) {
 			this.body.style.top = '';
 			this.body.style.left = '';
 
-			if (cont.nodeType && cont.nodeType == 1) {
-				this.container.appendChild(cont);
-			} else {
-				this.container.innerHTML = cont;
-			}
+			if (cont.nodeType && cont.nodeType == 1) this.container.appendChild(cont);else this.container.innerHTML = cont;
 
 			this.w = this.body.offsetWidth;
 			this.h = this.body.offsetHeight;
 
 			var coords = this['pos_' + pos](this.w, this.h);
-			if (coords) {
-				this.move(coords.x, coords.y);
-			}
+			if (coords) this.move(coords.x, coords.y);
 		},
 
+		// 隐藏tip
+		// is_instant：瞬间隐藏，没有延迟
 		hide: function hide(is_instant) {
 			if (!this.is_init || !this.is_showing) return !1;
+
+			//this.el_pending = null
 
 			function h() {
 				KCTip.body.classList.remove('on');
@@ -127,13 +157,10 @@
 				KCTip.pos = 'bottom';
 			}
 
-			if (this.pos == 'mouse') {
-				requestAnimationFrame(h);
-			} else {
-				this.timeout_fade = setTimeout(h, is_instant ? 0 : this.countdown_fade);
-			}
+			if (this.pos == 'mouse') requestAnimationFrame(h);else this.timeout_fade = setTimeout(h, is_instant ? 0 : this.countdown_fade);
 		},
 
+		// 完全隐藏
 		hideAfter: function hideAfter() {
 			this.body.classList.remove('show');
 			this.body.style.top = '';
@@ -146,6 +173,7 @@
 			_y = null;
 		},
 
+		// 格式化tip内容
 		content: function content(cont, el) {
 			el = el || this.el;
 
@@ -178,6 +206,7 @@
 			return cont;
 		},
 
+		// 读取内容
 		load: function load(t, i) {
 			this.cache.loading.innerHTML = '载入中...';
 
@@ -190,16 +219,19 @@
 			document.head.appendChild(script);
 		},
 
+		// 移动tip到 x,y
 		move: function move(x, y) {
 			this.body.style.top = y + 'px';
 			this.body.style.left = x + 'px';
 			this.body.classList.add('on');
 		},
 
+		// 获取小箭头尺寸
 		get_indicator_size: function get_indicator_size() {
 			return this.size_indicator;
 		},
 
+		// tip位置函数
 		pos_mouse: function pos_mouse(w, h) {
 			this.el.addEventListener('mousemove', KCTipMouseMoveHandler);
 		},
@@ -209,7 +241,6 @@
 			    y = o.top + this.el.offsetHeight + this.get_indicator_size();
 
 			this.body.setAttribute('kctip-indicator-pos', 'top');
-
 			return this.checkpos(x, y, w, h);
 		},
 		pos_top: function pos_top(w, h) {
@@ -218,7 +249,6 @@
 			    y = o.top - h - this.get_indicator_size();
 
 			this.body.setAttribute('kctip-indicator-pos', 'bottom');
-
 			return this.checkpos(x, y, w, h);
 		},
 		pos_left: function pos_left(w, h) {
@@ -227,7 +257,6 @@
 			    y = o.top + (this.el.offsetHeight - this.body.offsetHeight) / 2;
 
 			this.body.setAttribute('kctip-indicator-pos', 'right');
-
 			return this.checkpos(x, y, w, h);
 		},
 		pos_right: function pos_right(w, h) {
@@ -236,7 +265,6 @@
 			    y = o.top + (this.el.offsetHeight - this.body.offsetHeight) / 2;
 
 			this.body.setAttribute('kctip-indicator-pos', 'left');
-
 			return this.checkpos(x, y, w, h);
 		},
 		checkpos: function checkpos(x, y, w, h) {
@@ -252,6 +280,7 @@
 			w = w || this.w;
 			h = h || this.h;
 
+			// 超出X轴右边界
 			if (x + w > clientWidth + scrollLeft) {
 				if (w > o.left) {
 					pos = {
@@ -259,11 +288,37 @@
 						'y': y
 					};
 				} else {
+					//nx = o.left - w;
 					pos = this['pos_left'](w, h);
 				}
-			} else if (x < 0) pos = this['pos_right'](w, h);
+			}
 
-			if (y + h > scrollTop + clientHeight) pos = this['pos_top'](w, h);else if (y < scrollTop) pos = this['pos_bottom'](w, h);
+			// 超出X轴左边界
+			else if (x < 0)
+					//nx = 15;
+					pos = this['pos_right'](w, h);
+
+			// 超出Y轴下边界
+			if (y + h > scrollTop + clientHeight)
+				//ny = this.pos == 'bottom' ? ( o.top - this.el.outerHeight() ) : ( $(window).scrollTop() + $(window).height() - h );
+				pos = this['pos_top'](w, h);
+
+				// Node on top of viewport scroll
+				//else if ((o.top - 100) < $(window).scrollTop())
+				//	ny = o.top + this.el.outerHeight();
+
+				// Less than y viewport scrolled
+				//else if (y < $(window).scrollTop())
+				//	ny = $(window).scrollTop() + 10;
+
+				// Less than y viewport
+				//else if (y < 0)
+				//	ny = 15;
+
+				// 超出Y轴上边界
+			else if (y < scrollTop)
+					//ny = this.pos == 'bottom' ? ( o.top - this.el.outerHeight() ) : ( $(window).scrollTop() + $(window).height() - h );
+					pos = this['pos_bottom'](w, h);
 
 			this.body.setAttribute('kctip-indicator-offset-x', x - nx + 'px');
 			this.body.setAttribute('kctip-indicator-offset-y', y - ny + 'px');
@@ -276,6 +331,7 @@
 		}
 	};
 
+	/* check element matches query selector */
 	function elmatches(elm, selector) {
 		var matches = (elm.document || elm.ownerDocument).querySelectorAll(selector),
 		    i = matches.length;
@@ -283,6 +339,8 @@
 		return i > -1;
 	}
 
+	/* plain js */
+	/* https://plainjs.com/ */
 	function offset(el) {
 		var rect = el.getBoundingClientRect(),
 		    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -293,9 +351,7 @@
 	function KCTipMouseMoveHandler(e) {
 		_x = e.clientX;
 		_y = e.clientY;
-		if (!_isMoving) {
-			requestAnimationFrame(KCTipMouseMove);
-		}
+		if (!_isMoving) requestAnimationFrame(KCTipMouseMove);
 		_isMoving = !0;
 	}
 	function KCTipMouseMove() {
@@ -307,13 +363,16 @@
 		    x = _x + 10 + scrollLeft,
 		    y = _y + 25 + scrollTop;
 
+		// 超出X轴右边界
 		if (x + KCTip.w + 10 > clientWidth + scrollLeft) x = clientWidth + scrollLeft - KCTip.w - 10;
 
+		// 超出Y轴下边界
 		if (y + KCTip.h + 10 > clientHeight + scrollTop) y = clientHeight + scrollTop - KCTip.h - 10;
 
 		return KCTip.move(x, y);
 	}
 
+	/* delegate event */
 	var querySelector = '[href^="http://fleet.diablohu.com/"], [kctip]',
 	    eventTipshow = document.createEvent('Event'),
 	    eventTiphide = document.createEvent('Event');
--- source/kctip.js
+++ source/kctip.js
@@ -7,46 +7,61 @@ Current status
 * Browser compatibility NYI
 
  */
+			
+/*
+div#kctip.kctip
+	div.wrapper
+		CONTENT
+*/
+
+/*
+polyfill to do
+
+* transition event
+* classList
+* requestAnimationFrame
+*/
 
 
 (function(){
 	"use strict";
 	
 	let _isMoving = false
-		,_x, _y
+		,_x, _y;
 	
 	/* style */
-		let style = document.createElement('style')
-		style.innerHTML = '[[STYLE]]'
-		document.head.appendChild( style )
+		// STYLE will be replaced with CSS
+		let style = document.createElement('style');
+		style.innerHTML = '[[STYLE]]';
+		document.head.appendChild( style );
 
 
-	/* prevent touch triggering mouseenter */
+	/* makesure mouseover triggered by mouse not touch */
 		let _preventMouseover = false
-			,_isTouch = false
+			,_isTouch = false;
 		
 		function touchstartPreventMouseover(e){
-				_preventMouseover = true
-				_isTouch = true
+				_preventMouseover = true;
+				_isTouch = true;
 			}
-		document.addEventListener("touchstart", touchstartPreventMouseover)
+		document.addEventListener("touchstart", touchstartPreventMouseover);
 		document.addEventListener("pointerenter", function pointerenterPreventMouseover(e){
 				if( e.pointerType == 'touch' )
-					touchstartPreventMouseover()
+					touchstartPreventMouseover();
 				else{
-					_preventMouseover = false
-					_isTouch = false
+					_preventMouseover = false;
+					_isTouch = false;
 				}
-			})
+			});
 		//document.addEventListener("mouseenter", function mouseoverPreventMouseover(e){
 		document.addEventListener("mouseover", function mouseoverPreventMouseover(e){
 				if( _isTouch ){
-					_isTouch = false
-					_preventMouseover = true
+					_isTouch = false;
+					_preventMouseover = true;
 				}else{
-					_preventMouseover = false
+					_preventMouseover = false;
 				}
-			})
+			});
 		//document.addEventListener("mouseleave", function mouseleavePreventMouseover(e){
 		//	})
 
@@ -54,190 +69,177 @@ Current status
 	let KCTip = {
 		//is_init:			false,
 		//is_showing:		false,
+		// curLoading: 		null,
 		pos:				'bottom',
 		//pos:				'mouse',
 		//w:				0,
 		//h:				0,
 		size_indicator:		8,
-	
-		// 文本滤镜，可任意加入滤镜函数
-		filters: [],
-	
-		// 隐藏tip延迟时间，毫秒
-		countdown_fade:			250,
-		
-		// 结果缓存
 		cache: {},
 	
-		// curLoading: 			null,
+		// content filters
+		filters: [],
 	
-		// 初始化tip
+		// delay to hide tip
+		countdown_fade:			250,
+	
 		init: function(){
 			if(this.is_init)
-				return this
+				return this;
 			
-			this.body = document.createElement('div')
-			this.body.classList.add('kctip')
-			this.body.setAttribute('id', 'kctip')
-			let evts = ['transitionend','webkitTransitionEnd','mozTransitionEnd']
+			this.body = document.createElement('div');
+			this.body.classList.add('kctip');
+			this.body.setAttribute('id', 'kctip');
+			let evts = ['transitionend','webkitTransitionEnd','mozTransitionEnd'];
 			evts.forEach(function(evt){
 				this.body.addEventListener(evt, function(e) {
 					let style = window.getComputedStyle ? getComputedStyle(e.target, null) : e.target.currentStyle;
-					if( e.currentTarget == e.target && e.propertyName == 'opacity' && style.opacity == 0 ){
-						KCTip.hideAfter()
-					}
+					if( e.currentTarget == e.target && e.propertyName == 'opacity' && style.opacity == 0 )
+						KCTip.hideAfter();
 				})
 			}, this)
-			document.body.appendChild( this.body )
+			document.body.appendChild( this.body );
 			
-			this.container = document.createElement('div')
-			this.container.classList.add('wrapper')
-			this.body.appendChild( this.container )
+			this.container = document.createElement('div');
+			this.container.classList.add('wrapper');
+			this.body.appendChild( this.container );
 	
-			// 虚化背景
-				if( 'backdrop-filter' in document.documentElement.style || '-webkit-backdrop-filter' in document.documentElement.style ){
-					this.body.addClass('mod-blur-backdrop')
-				}
+			// backdrop-filter
+				if( 'backdrop-filter' in document.documentElement.style || '-webkit-backdrop-filter' in document.documentElement.style )
+					this.body.addClass('mod-blur-backdrop');
 	
-			this.is_init=true
+			this.is_init=true;
 		},
 	
 		// 显示
-		// cont:	HTML内容
-		// el:		触发tip的DOM，通常为鼠标指向位置
-		// pos:		tip位置
+		// el:		element that trigger the tip
+		// cont: 	HTML code or element node
+		// pos:		tip position, top || bottom || right || left
 		show: function( el, cont, pos ){
 			if( _preventMouseover || !el )
-				return false
+				return false;
 	
-			clearTimeout(this.timeout_fade)
+			clearTimeout(this.timeout_fade);
 	
 			el = el || document.body;
-			this.el = el
-	
-			pos = pos || el.getAttribute('kctip-position') || this.pos
-	
-			cont = this.content(cont)
-	
+			this.el = el;
+			pos = pos || el.getAttribute('kctip-position') || this.pos;
+			cont = this.content(cont);
+
 			this.init();
 	
-			if( !this.body.classList.contains('show') ){
-				this.body.classList.add('show')
-			}
+			if( !this.body.classList.contains('show') )
+				this.body.classList.add('show');
 	
-			this.position( cont, pos );
-	
+			this.position( cont, pos );	
 			this.is_showing=true;
 		},
 	
 		// 计算tip位置
 		position:function(cont, pos){
-			this.body.style.top = ''
-			this.body.style.left = ''
+			this.body.style.top = '';
+			this.body.style.left = '';
 
-			if( cont.nodeType && cont.nodeType == 1 ){
-				this.container.appendChild(cont)
-			}else{
-				this.container.innerHTML = cont
-			}
+			if( cont.nodeType && cont.nodeType == 1 )
+				this.container.appendChild(cont);
+			else
+				this.container.innerHTML = cont;
 
-			this.w = this.body.offsetWidth
-			this.h = this.body.offsetHeight
+			this.w = this.body.offsetWidth;
+			this.h = this.body.offsetHeight;
 	
 			let coords = this['pos_'+pos]( this.w , this.h );
-			if(coords){
-				this.move(coords.x, coords.y)
-			}
+			if(coords)
+				this.move(coords.x, coords.y);
 		},
 	
 		// 隐藏tip
 		// is_instant：瞬间隐藏，没有延迟
 		hide:function( is_instant ){
 			if( !this.is_init || !this.is_showing )
-				return false
+				return false;
 	
 			//this.el_pending = null
 			
 			function h(){
-				KCTip.body.classList.remove('on')
-				KCTip.el.removeEventListener(KCTipMouseMoveHandler)
-				KCTip.el = null
-				KCTip.is_showing = false
-				KCTip.pos = 'bottom'
+				KCTip.body.classList.remove('on');
+				KCTip.el.removeEventListener(KCTipMouseMoveHandler);
+				KCTip.el = null;
+				KCTip.is_showing = false;
+				KCTip.pos = 'bottom';
 			}
 			
-			if( this.pos == 'mouse' ){
-				requestAnimationFrame(h)
-			}else{
-				this.timeout_fade = setTimeout(h, is_instant ? 0 : this.countdown_fade)
-			}
+			if( this.pos == 'mouse' )
+				requestAnimationFrame(h);
+			else
+				this.timeout_fade = setTimeout(h, is_instant ? 0 : this.countdown_fade);
 		},
 		
 		// 完全隐藏
 		hideAfter: function(){
-			this.body.classList.remove('show')
-			this.body.style.top = ''
-			this.body.style.left = ''
-			this.body.removeAttribute('kctip-indicator-pos')
-			this.body.removeAttribute('kctip-indicator-offset-x')
-			this.body.removeAttribute('kctip-indicator-offset-y')
-			delete this.curLoading
-			_x = null
-			_y = null
+			this.body.classList.remove('show');
+			this.body.style.top = '';
+			this.body.style.left = '';
+			this.body.removeAttribute('kctip-indicator-pos');
+			this.body.removeAttribute('kctip-indicator-offset-x');
+			this.body.removeAttribute('kctip-indicator-offset-y');
+			delete this.curLoading;
+			_x = null;
+			_y = null;
 		},
 		
 		// 格式化tip内容
 		content: function( cont, el ){
-			el = el || this.el
+			el = el || this.el;
 			
-			let t, i
+			let t, i;
 			
 			if( !cont ){
-				cont = el.getAttribute('href')
-				let matches = /\/([a-z]+)\/([0-9]+)/gi.exec(cont)
+				cont = el.getAttribute('href');
+				let matches = /\/([a-z]+)\/([0-9]+)/gi.exec(cont);
 				if( matches && matches.length > 1 ){
-					t = matches[1]
-					i = matches[2]
+					t = matches[1];
+					i = matches[2];
 				}
 			}
 			
 			if( t && i ){
 				if( !this.cache[t] )
-					this.cache[t] = {}
+					this.cache[t] = {};
 
 				if( this.cache[t][i] )
-					return this.cache[t][i]
+					return this.cache[t][i];
 				
-				this.curLoading = t + '::' + i
+				this.curLoading = t + '::' + i;
 				if( !this.cache.loading ){
-					this.cache.loading = document.createElement('div')
-					this.cache.loading.classList.add('loading')
+					this.cache.loading = document.createElement('div');
+					this.cache.loading.classList.add('loading');
 				}
-				this.load(t, i)
-				return this.cache.loading
+				this.load(t, i);
+				return this.cache.loading;
 			}
 	
-			return cont
+			return cont;
 		},
 		
 		// 读取内容
 		load: function(t, i){
-			this.cache.loading.innerHTML = '载入中...'
+			this.cache.loading.innerHTML = '载入中...';
 			
-			let script = document.createElement('script')
-			script.src = 'http://fleet.diablohu.com/!/tip/'+t+'/'+i+'.js'
+			let script = document.createElement('script');
+			script.src = 'http://fleet.diablohu.com/!/tip/'+t+'/'+i+'.js';
 			script.addEventListener('error', function(e) {
-				KCTip.cache.loading.innerHTML = '发生错误...'
-			})
+				KCTip.cache.loading.innerHTML = '发生错误...';
+			});
 			
-			document.head.appendChild( script )
+			document.head.appendChild( script );
 		},
 	
 		// 移动tip到 x,y
 		move: function(x,y){
-			this.body.style.top = y + 'px'
-			this.body.style.left = x + 'px'
-			this.body.classList.add('on')
+			this.body.style.top = y + 'px';
+			this.body.style.left = x + 'px';
+			this.body.classList.add('on');
 		},
 	
 		// 获取小箭头尺寸
@@ -247,43 +249,39 @@ Current status
 	
 		// tip位置函数
 		pos_mouse: function(w,h){
-			this.el.addEventListener('mousemove', KCTipMouseMoveHandler)
+			this.el.addEventListener('mousemove', KCTipMouseMoveHandler);
 		},
 		pos_bottom: function(w,h){
 			let o	= offset(this.el)
 				,x	= o.left + ( this.el.offsetWidth - this.body.offsetWidth )/2
-				,y	= o.top + this.el.offsetHeight + this.get_indicator_size()
+				,y	= o.top + this.el.offsetHeight + this.get_indicator_size();
 	
-			this.body.setAttribute('kctip-indicator-pos', 'top' )
-	
-			return this.checkpos(x,y,w,h)
+			this.body.setAttribute('kctip-indicator-pos', 'top' );
+			return this.checkpos(x,y,w,h);
 		},
 		pos_top: function(w,h){
 			let o	= offset(this.el)
 				,x	= o.left + ( this.el.offsetWidth - this.body.offsetWidth )/2
-				,y	= o.top - h - this.get_indicator_size()
+				,y	= o.top - h - this.get_indicator_size();
 	
-			this.body.setAttribute('kctip-indicator-pos', 'bottom' )
-	
-			return this.checkpos(x,y,w,h)
+			this.body.setAttribute('kctip-indicator-pos', 'bottom' );
+			return this.checkpos(x,y,w,h);
 		},
 		pos_left: function(w,h){
 			let o	= offset(this.el)
 				,x	= o.left - w - this.get_indicator_size()
-				,y	= o.top + ( this.el.offsetHeight - this.body.offsetHeight )/2
+				,y	= o.top + ( this.el.offsetHeight - this.body.offsetHeight )/2;
 	
-			this.body.setAttribute('kctip-indicator-pos', 'right' )
-	
-			return this.checkpos(x,y,w,h)
+			this.body.setAttribute('kctip-indicator-pos', 'right' );
+			return this.checkpos(x,y,w,h);
 		},
 		pos_right: function(w,h){
 			let o	= offset(this.el)
 				,x	= o.left + this.el.offsetWidth + this.get_indicator_size()
-				,y	= o.top + ( this.el.offsetHeight - this.body.offsetHeight )/2
+				,y	= o.top + ( this.el.offsetHeight - this.body.offsetHeight )/2;
 	
-			this.body.setAttribute('kctip-indicator-pos', 'left' )
-	
-			return this.checkpos(x,y,w,h)
+			this.body.setAttribute('kctip-indicator-pos', 'left' );
+			return this.checkpos(x,y,w,h);
 		},
 		checkpos: function(x,y,w,h){
 			let o 		= offset(this.el)
@@ -294,10 +292,10 @@ Current status
 				,clientWidth = document.documentElement.clientWidth
 				,clientHeight = document.documentElement.clientHeight
 				,scrollLeft	= window.pageXOffset || document.documentElement.scrollLeft
-				,scrollTop 	= window.pageYOffset || document.documentElement.scrollTop
+				,scrollTop 	= window.pageYOffset || document.documentElement.scrollTop;
 			
-			w = w || this.w
-			h = h || this.h
+			w = w || this.w;
+			h = h || this.h;
 	
 			// 超出X轴右边界
 			if ((x + w) > clientWidth + scrollLeft ){
@@ -305,22 +303,22 @@ Current status
 					pos = {
 						'x': clientWidth + scrollLeft - w - 2,
 						'y': y
-					}
+					};
 				}else{
 					//nx = o.left - w;
-					pos = this['pos_left']( w , h )
+					pos = this['pos_left']( w , h );
 				}
 			}
 	
 			// 超出X轴左边界
 			else if (x < 0)
 				//nx = 15;
-				pos = this['pos_right']( w , h )
+				pos = this['pos_right']( w , h );
 	
 			// 超出Y轴下边界
 			if ( (y + h) > (scrollTop + clientHeight) )
 				//ny = this.pos == 'bottom' ? ( o.top - this.el.outerHeight() ) : ( $(window).scrollTop() + $(window).height() - h );
-				pos = this['pos_top']( w , h )
+				pos = this['pos_top']( w , h );
 	
 			// Node on top of viewport scroll
 			//else if ((o.top - 100) < $(window).scrollTop())
@@ -337,16 +335,16 @@ Current status
 			// 超出Y轴上边界
 			else if ( y < scrollTop )
 				//ny = this.pos == 'bottom' ? ( o.top - this.el.outerHeight() ) : ( $(window).scrollTop() + $(window).height() - h );
-				pos = this['pos_bottom']( w , h )
+				pos = this['pos_bottom']( w , h );
 	
-			this.body.setAttribute('kctip-indicator-offset-x', (x - nx)+'px' )
-			this.body.setAttribute('kctip-indicator-offset-y', (y - ny)+'px' )
+			this.body.setAttribute('kctip-indicator-offset-x', (x - nx)+'px' );
+			this.body.setAttribute('kctip-indicator-offset-y', (y - ny)+'px' );
 
-			return pos
+			return pos;
 		},
 		
 		trigger_by_el: function(el){
-			this.show(el)
+			this.show(el);
 		}
 	}
 	
@@ -371,38 +369,37 @@ Current status
 
 
 	function KCTipMouseMoveHandler(e){
-		_x = e.clientX
-		_y = e.clientY
-		if( !_isMoving ){
+		_x = e.clientX;
+		_y = e.clientY;
+		if( !_isMoving )
 			requestAnimationFrame(KCTipMouseMove);
-		}
-		_isMoving = true
+		_isMoving = true;
 	}
 	function KCTipMouseMove(){
-		_isMoving = false
+		_isMoving = false;
 		let clientWidth = document.documentElement.clientWidth
 			,clientHeight = document.documentElement.clientHeight
 			,scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
 			,scrollTop = window.pageYOffset || document.documentElement.scrollTop
 			,x = _x + 10 + scrollLeft
-			,y = _y + 25 + scrollTop
+			,y = _y + 25 + scrollTop;
 	
 		// 超出X轴右边界
 			if ((x + KCTip.w + 10) > clientWidth + scrollLeft )
-				x = clientWidth + scrollLeft - KCTip.w - 10
+				x = clientWidth + scrollLeft - KCTip.w - 10;
 	
 		// 超出Y轴下边界
 			if ((y + KCTip.h + 10) > clientHeight + scrollTop )
-				y = clientHeight + scrollTop - KCTip.h - 10
+				y = clientHeight + scrollTop - KCTip.h - 10;
 
-		return KCTip.move(x,y)
+		return KCTip.move(x,y);
 	}
 
 
 	/* delegate event */
 		let querySelector = '[href^="http://fleet.diablohu.com/"], [kctip]'
 			,eventTipshow = document.createEvent('Event')
-			,eventTiphide = document.createEvent('Event')
+			,eventTiphide = document.createEvent('Event');
 		eventTipshow.initEvent('tipshow', true, true);
 		eventTiphide.initEvent('tiphide', true, true);
 		document.addEventListener("mouseover", function mouseoverKCTip(e) {
